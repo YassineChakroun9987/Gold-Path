f07363214cb3c6603d99e992b641b4ab964e7b40
Yassine Chakroun <158570270+YassineChakroun9987@users.noreply.github.com>
2025-12-02 23:45:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -617,7 +617,7 @@ def visualize_graph(graph, node_labels, title="Graph"):
     # --------------------------------------------------
     # 3. Streamlit container (NO CROPPING)
     # --------------------------------------------------
-    initial_zoom = 0.45      # <<< TRY VALUES: 0.45, 0.35, 0.25, 0.20, 0.15, 0.10
+    initial_zoom = 0.1     # <<< TRY VALUES: 0.45, 0.35, 0.25, 0.20, 0.15, 0.10
     initial_pan_x = -40        # <<< shift left/right (positive → right)
     initial_pan_y = 0        # <<< shift up/down (positive → down)
 
