c19b496ad0a4b38494136c819a64365da1bb5882
Yassine Chakroun <158570270+YassineChakroun9987@users.noreply.github.com>
2025-12-03 12:50:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -545,7 +545,7 @@ def visualize_graph(graph, node_labels, title="Graph"):
     dot.graph_attr.update({
         "overlap": "false",
         "splines": "true",
-        "dpi": "500",
+        "dpi": "1000",
         "ratio": "0.5",
         "size": "6!",
         "pad": "0.5",
