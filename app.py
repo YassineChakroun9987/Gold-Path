699a2988882f3f55237242d8ca53f5ec6a908b43
Yassine Chakroun <158570270+YassineChakroun9987@users.noreply.github.com>
2025-12-03 01:19:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -556,8 +556,8 @@ def visualize_graph(graph, node_labels, title="Graph"):
             style="filled",
             fillcolor=node_colors[str(i)],
             color="black",
-            fontsize="16",
-            width="1.0"
+            fontsize="10",
+            width="0.5"
         )
 
     for i in range(V):
